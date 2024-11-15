633930fc567ebcfc9aad2f568780c0d87e6c30cc
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-11-15 19:08:48 +0400

    Generete chapter content

--- app/create-course/[courseId]/page.jsx
+++ app/create-course/[courseId]/page.jsx
@@ -8,10 +8,14 @@ import React, { useEffect, useState } from "react";
 import CourseBasics from "./_components/CourseBasics";
 import CourseDetails from "./_components/CourseDetails";
 import ChapterList from "./_components/ChapterList";
+import { Button } from "@/components/ui/button";
+import { GenerateChapterContent_AI } from "@/config/AIModel";
+import LoadingDialog from "../_components/LoadingDialog";
 
 function CourseLayout({ params }) {
   const { user } = useUser();
   const [course, setCourse] = useState([]);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     console.log(params);
     params && GetCourse();
@@ -30,12 +34,42 @@ function CourseLayout({ params }) {
     setCourse(result[0]);
     console.log(result);
   };
+
+  const GenereteChapterContent = () => {
+    setLoading(true);
+    const chapters = course?.courseOutput?.chapters;
+
+    chapters.forEach(async (chapter, index) => {
+      const PROMPT =
+        "Explain the concept in details on topic:" +
+        course?.name +
+        " ,Chapter: " +
+        chapter?.chapter_name +
+        " , in JSON format with lsit of array with fiels as title,description in details, code Example(<precode> format) if applicable.";
+      console.log(PROMPT);
+      if (index <= 3) {
+        try {
+          const result = await GenerateChapterContent_AI.sendMessage(PROMPT);
+          console.log(result?.response?.text());
+
+          //Generate Video URL
+
+          // Save Chapter Content + Video URL
+          setLoading(false);
+        } catch (error) {
+          setLoading(false);
+          console.log(error);
+        }
+      }
+    });
+  };
   return (
     <div className="px-7 md:px-20 lg:px44">
       <h2 className="font-bold text-center text-5xl text-primary">
         Course Layout
       </h2>
 
+      <LoadingDialog loading={loading} />
       {/* Basic Info */}
       <CourseBasics course={course} refreshData={() => GetCourse()} />
 
@@ -44,6 +78,10 @@ function CourseLayout({ params }) {
 
       {/* List of Chapters or Lessons */}
       <ChapterList course={course} refreshData={() => GetCourse()} />
+
+      <Button onClick={GenereteChapterContent} className="my-10">
+        Gererate Course Content
+      </Button>
     </div>
   );
 }
